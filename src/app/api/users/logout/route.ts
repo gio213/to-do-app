515e4b62789b3a9d4481a3b7588d7a2b800bed81
Giorgi Patsia <gio.patsia@gmail.com>
2023-11-22 17:00:04 +0100

    wip

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -2,10 +2,13 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
+    console.log("Logout route reached");
     const response = NextResponse.json({ message: "Logout", success: true });
     response.cookies.set("token", "", { httpOnly: true, expires: new Date(0), path: "/", domain: "to-do-app-ivory-nu.vercel.app", sameSite: "lax" });
+    console.log("Cookie deleted");
     return response;
   } catch (error: any) {
+    console.error("Error in logout route:", error.message);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
