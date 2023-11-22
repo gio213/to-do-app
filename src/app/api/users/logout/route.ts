26b2843cddb999361adf4f46a4aadb619f0a1727
Giorgi Patsia <gio.patsia@gmail.com>
2023-11-22 16:46:12 +0100

    fixed logout route to delete toke when project is in production

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const response = NextResponse.json({ message: "Logout", success: true });
-    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0), path: "/", domain: "https://to-do-app-ivory-nu.vercel.app/" });
     return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
